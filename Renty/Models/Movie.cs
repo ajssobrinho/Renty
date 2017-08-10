b5fd7e1be399e114ffae73289cca59f3127ce1ba
André Redondo <aredondo@agap2.pt>
2017-08-10 17:20:45 +0100

    movies variables added

---
+++ Renty/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Renty.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public String Type { get; set; }
+
+    }
+}
